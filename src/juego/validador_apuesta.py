352fe0ed059e656581d29d7cb345157a627de31a
khewa11 <jumana2022@udec.cl>
2025-08-30 12:31:59 -0400

    RED: Test para validar apuestas fallidos

---
+++ src/juego/validador_apuesta.py
@@ -0,0 +1,9 @@
+class validador_apuesta:
+    def __init__(self):
+        self.ronda_especial = False
+
+    def activar_ronda_especial(self):
+        self.ronda_especial = True
+
+    def validar_apuesta(self, apuesta_nueva, apuesta_actual):
+        return None
---
+++ tests/test_validador_apuesta.py
@@ -0,0 +1,74 @@
+import pytest
+import sys
+sys.path.append("src")
+from juego.validador_apuesta import validador_apuesta
+
+@pytest.fixture
+def validador():
+    return validador_apuesta()
+
+## casos normales normal-normal
+def test_apuesta_valida_cantidad(validador):
+    actual = (2,3) ### formato (pinta, cantidad)
+    apuesta = (2,4)
+    resultado = validador.validar_apuesta(apuesta, actual)
+    assert resultado == True
+
+def test_apuesta_invalida_cantidad(validador):
+    actual = (2,3) ### formato (pinta, cantidad)
+    apuesta = (2,2)
+    resultado = validador.validar_apuesta(apuesta, actual)
+    assert resultado == False
+
+def test_apuesta_valida_pinta(validador):
+    actual = (2,3) ### formato (pinta, cantidad)
+    apuesta = (3,1)
+    resultado = validador.validar_apuesta(apuesta, actual)
+    assert resultado == True
+
+def test_apuesta_invalida_pinta(validador):
+    actual = (4,3) ### formato (pinta, cantidad)
+    apuesta = (3,3)
+    resultado = validador.validar_apuesta(apuesta, actual)
+    assert resultado == False
+
+## caso especial de los As-normal
+def test_apuesta_valida_as_a_normal(validador):
+    actual = (1,3)
+    apuesta = (2,7)
+    resultado = validador.validar_apuesta(apuesta, actual)
+    assert resultado == True
+
+def test_apuesta_invalida_as_a_normal(validador):
+    actual = (1,3)
+    apuesta = (2,6)
+    resultado = validador.validar_apuesta(apuesta, actual)
+    assert resultado == False
+
+### casos normales normal-As###
+def test_apuesta_valida_normal_a_as(validador):
+    actual =  (3,5)
+    apuesta = (1,3)
+    resultado = validador.validar_apuesta(apuesta, actual)
+    assert resultado == True
+
+def test_apuesta_invalida_normal_a_as(validador):
+    actual =  (3,5)
+    apuesta = (1,2)
+    resultado = validador.validar_apuesta(apuesta, actual)
+    assert resultado == False
+
+### casos especiales As-As
+def test_apuesta_valida_as_a_as(validador):
+    actual = (1,4)
+    apuesta = (1,5)
+    resultado = validador.validar_apuesta(apuesta, actual)
+    assert resultado == True
+
+def test_apuesta_invalida_as_a_as(validador):
+    actual = (1,4)
+    apuesta = (1,4)
+    resultado = validador.validar_apuesta(apuesta, actual)
+    assert resultado == False
+
+
